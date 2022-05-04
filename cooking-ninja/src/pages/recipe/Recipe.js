511a2125ca50f6c69dc00b59962fe342c09251dd
duongyeudoi <19020265@vnu.edu.vn>
2022-05-04 10:05:57 +0700

    fetch a single document

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -1,15 +1,34 @@
-import { useParams } from 'react-router-dom'
-import { useFetch } from '../../hooks/useFetch'
-import { useTheme } from '../../hooks/useTheme'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useTheme } from '../../hooks/useTheme';
+import { projectFirestore } from '../../firebase/config';
 
 // styles
-import './Recipe.css'
+import './Recipe.css';
 
 export default function Recipe() {
-  const { id } = useParams()
-  const url = 'http://localhost:3000/recipes/' + id
-  const { error, isPending, data: recipe } = useFetch(url)
-  const { mode } = useTheme()
+  const { id } = useParams();
+  const { mode } = useTheme();
+  const [recipe, setRecipe] = useState(null);
+  const [isPending, setIsPending] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+    projectFirestore
+      .collection('recipes')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError('Could not find data');
+        }
+      });
+  }, [id]);
 
   return (
     <div className={`recipe ${mode}`}>
@@ -20,11 +39,13 @@ export default function Recipe() {
           <h2 className="page-title">{recipe.title}</h2>
           <p>Takes {recipe.cookingTime} to cook.</p>
           <ul>
-            {recipe.ingredients.map(ing => <li key={ing}>ing</li>)}
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>ing</li>
+            ))}
           </ul>
           <p className="method">{recipe.method}</p>
         </>
       )}
     </div>
-  )
+  );
 }
